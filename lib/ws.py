d748d27522e8aea81163e9a95d6e07eaa54f643d
xeniality <ohZeGiX@gmail.com>
2019-09-10 22:32:52 +0300

    Fix on_close

--- lib/ws.py
+++ lib/ws.py
@@ -8,7 +8,7 @@ class Websocket:
         headers = {"Cookie": cookie, "User-Agent": "tradesniper_poe"}
 
         print("INFO: Requesting websocket handhake for {}".format(query))
-        socket = websocket.WebSocketApp(url, header=headers, on_message=self.message_handler, on_error=self.error_handler, on_close=self.error_handler, on_open=self.on_open, on_ping=self.on_ping)
+        socket = websocket.WebSocketApp(url, header=headers, on_message=self.message_handler, on_error=self.error_handler, on_close=self.on_close, on_open=self.on_open, on_ping=self.on_ping)
         socket.run_forever()
 
     def on_ping(self):
@@ -16,6 +16,9 @@ class Websocket:
 
     def on_open(self):
         print("INFO: Websocket handshake succesful")
+
+    def on_close(self):
+        print("INFO: Websocket closed")
 
     def error_handler(self, e):
         print("ERROR:", e)
